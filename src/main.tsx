fae5626eb93723f3eedd43270f829e0985517d61
Khalid Elshafie <abolkog@gmail.com>
2025-06-03 15:37:18 +1000

    chore: add missing basename on browser router

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <main className="bg-black-400 text-gray-400 h-screen flex flex-col items-center overflow-auto">
       <section className="w-full sm:w-[60%] lg:w-[60%] flex flex-col items-center justify-center gap-10 min-h-screen">
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.PROD ? "/thinkdots/" : "/"}>
           <GameProvider>
             <App />
           </GameProvider>

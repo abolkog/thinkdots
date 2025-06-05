56bf503148a18a13596cf2f137186bd3bb256fdf
Khalid Elshafie <abolkog@gmail.com>
2025-06-06 08:02:02 +1000

    refactor: display all guess rows at once instead of one by one

--- src/components/game/GameBoard.test.tsx
+++ src/components/game/GameBoard.test.tsx
@@ -2,9 +2,10 @@ import { render, screen } from '@testing-library/react';
 import GameBoard from '@components/game/GameBoard';
 import { GameContext } from '@context/GameContext';
 import { mockState } from '@test/fixtures';
+import { NUMBER_OF_ATTEMPTS } from '@util/common';
 
 describe('GameBoard', () => {
-  it('renders a row for each guess', () => {
+  it('renders correct number of row ', () => {
     render(
       <GameContext.Provider value={{ state: mockState, dispatch: jest.fn() }}>
         <GameBoard />
@@ -12,19 +13,6 @@ describe('GameBoard', () => {
     );
 
     const gameBoard = screen.getByTestId('gameBoard');
-    expect(gameBoard.children.length).toEqual(mockState.guessNumber);
-  });
-
-  it('renders the correct colors for each guess', () => {
-    render(
-      <GameContext.Provider value={{ state: mockState, dispatch: jest.fn() }}>
-        <GameBoard />
-      </GameContext.Provider>
-    );
-    mockState.guesses.forEach((color) => {
-      if (color) {
-        expect(screen.getAllByTestId(`dot-${color}`).length).toBeGreaterThan(0);
-      }
-    });
+    expect(gameBoard.children.length).toEqual(NUMBER_OF_ATTEMPTS);
   });
 });
--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -9,7 +9,7 @@ function Game() {
     <section className="w-full sm:w-[60%] lg:w-[90%] flex flex-col justify-center items-center">
       <Header />
 
-      <div className="flex flex-col items-center justify-center w-full bg-black-300 p-5 mb-10 rounded-2x">
+      <div className="flex flex-col items-center justify-center max-w-96 bg-black-300 p-5 mb-10 rounded-4xl">
         <GameBoard />
       </div>
       <Controls />

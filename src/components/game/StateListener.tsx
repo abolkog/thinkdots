b09177f65a932f0f2e2e3f0d5dc97c336199a841
Khalid Elshafie <abolkog@gmail.com>
2025-06-06 16:19:20 +1000

    feat: add sound effect

--- src/components/game/StateListener.tsx
+++ src/components/game/StateListener.tsx
@@ -1,36 +1,55 @@
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router';
 import { AppActions } from '@context/reducer';
 import { useGameContext } from '@hooks/useGameContext';
 import { NUMBER_OF_ATTEMPTS } from '@util/common';
+import { useSound } from '@hooks/useSound';
 
 export default function StateListener() {
   const navigate = useNavigate();
-
+  const { stopBg, playBg, playGameOver, playWin } = useSound();
   const { state, dispatch } = useGameContext();
   const { isGameOver, guessNumber } = state;
+  const triggeredRef = useRef(false);
 
   const resetGame = useCallback(() => {
+    playBg();
     dispatch({
       type: AppActions.RESET_GAME,
       payload: {
         isOpen: false,
       },
     });
-  }, [dispatch]);
+  }, [dispatch, playBg]);
 
   const exitGame = useCallback(() => {
     resetGame();
     navigate('/');
   }, [resetGame, navigate]);
 
-  useEffect(() => {
+  const manageSound = useCallback(() => {
+    stopBg();
     if (isGameOver) {
+      playWin();
+    } else {
+      playGameOver();
+    }
+  }, [isGameOver, playGameOver, playWin, stopBg]);
+
+  useEffect(() => {
+    if (
+      (isGameOver || guessNumber > NUMBER_OF_ATTEMPTS) &&
+      !triggeredRef.current
+    ) {
+      manageSound();
+      triggeredRef.current = true;
       dispatch({
         type: AppActions.TOGGLE_MODAL,
         payload: {
-          title: 'Congratulations!',
-          message: `You guessed the secret code in ${guessNumber} attempts!`,
+          title: isGameOver ? 'Congratulations!' : 'Game Over',
+          message: isGameOver
+            ? `You guessed the secret code in ${guessNumber} attempts!`
+            : 'You have used all your guesses. Better luck next time!',
           yesButtonText: 'Play Again',
           noButtonText: 'Exit',
           yesButtonOnClick: resetGame,
@@ -40,21 +59,10 @@ export default function StateListener() {
       });
     }
 
-    if (guessNumber > NUMBER_OF_ATTEMPTS) {
-      dispatch({
-        type: AppActions.TOGGLE_MODAL,
-        payload: {
-          title: 'Game Over',
-          message: 'You have used all your guesses. Better luck next time!',
-          yesButtonText: 'Play Again',
-          noButtonText: 'Exit',
-          yesButtonOnClick: resetGame,
-          noButtonOnClick: exitGame,
-          isOpen: true,
-        },
-      });
+    if (!isGameOver && guessNumber === 1) {
+      triggeredRef.current = false;
     }
-  }, [dispatch, exitGame, guessNumber, isGameOver, resetGame]);
+  }, [dispatch, exitGame, guessNumber, isGameOver, resetGame, manageSound]);
 
   return null;
 }
---
+++ src/hooks/useSound.tsx
@@ -0,0 +1,22 @@
+import { Howl } from 'howler';
+
+const sounds = {
+  bg: new Howl({ src: ['/sounds/bg.mp3'], loop: true, volume: 0.3 }),
+  setColor: new Howl({ src: ['/sounds/pop.mp3'], loop: false, volume: 0.5 }),
+  gameOver: new Howl({
+    src: ['/sounds/game_over.mp3'],
+    loop: false,
+    volume: 0.5,
+  }),
+  win: new Howl({ src: ['/sounds/win.mp3'], loop: false, volume: 0.5 }),
+};
+
+export function useSound() {
+  return {
+    playBg: () => sounds.bg.play(),
+    stopBg: () => sounds.bg.stop(),
+    playSetColor: () => sounds.setColor.play(),
+    playGameOver: () => sounds.gameOver.play(),
+    playWin: () => sounds.win.play(),
+  };
+}
